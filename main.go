9308017083b06d67e8241c64b5c1dc1bbf7438da
Son Nguyen Van <son.nguyenvan@pycogroup.com>
2020-01-30 21:04:02 +0700

    feat: get league

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 )
 
 // NewInMemoryPlayerStore is InMemoryPlayerStore instance
@@ -27,20 +25,19 @@ func (i *InMemoryPlayerStore) RecordWin(name string) {
 	i.store[name]++
 }
 
-func main() {
-	server := &PlayerServer{NewInMemoryPlayerStore()}
-	if err := http.ListenAndServe(GetPort(), server); err != nil {
-		log.Fatalf("could not listen on port %s %v", GetPort(), err)
+// GetLeague is show the list of player with wins
+func (i *InMemoryPlayerStore) GetLeague() []Player {
+	var league []Player
+
+	for name, wins := range i.store {
+		league = append(league, Player{name, wins})
 	}
+	return league
 }
 
-// GetPort from ENV
-func GetPort() string {
-	var port = os.Getenv("PORT")
-	// Set a default port if there is nothing in the environment
-	if port == "" {
-		port = "3000"
-		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+func main() {
+	server := NewPlayerServer(NewInMemoryPlayerStore())
+	if err := http.ListenAndServe(":5000", server); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
 	}
-	return ":" + port
 }
